148947622f68e28532ae080ed2689c6d44fde0b3
tiagolofi <jejeco75@gmail.com>
2021-09-17 14:22:04 -0300

    update finaceiro

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 from pandas import read_excel
 from statistics import mode
 from re import sub, findall
-from pandas import concat
+from pandas import concat, isna
 import warnings
 import base64
 
@@ -266,6 +266,8 @@ def sigef3(file: str, skip: int, range_cols: str):
 	df['Unnamed: 11'] = df['Unnamed: 11'].astype(float)	
 
 	df.columns = colnames
+
+	df = df[~isna(df['N_EMPENHO'])]
 
 	return df
 

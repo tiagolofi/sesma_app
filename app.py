cfdd0cdd982d0e9c4103d0a14b4f06be59149171
tiagolofi <jejeco75@gmail.com>
2022-12-03 11:00:25 -0300

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-from utils import fns, pagamento, extrato, listar_ordem, nota_empenho_celula, observacoes, situacao_pp, orc, listar_pre_empenho, nota_pre_empenho_celula, deta_conta, export_excel
+from utils import * # fns, pagamento, extrato, listar_ordem, nota_empenho_celula, observacoes, situacao_pp, orc, listar_pre_empenho, nota_pre_empenho_celula, deta_conta, export_excel
 from datetime import datetime
 
 icon = 'https://bluefocus.com.br/sites/default/files/styles/medium/public/icon-financeiro.png'

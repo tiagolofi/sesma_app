f7480ae0ff27d310c8fb01a5fb5957b7257c8f16
tiagolofi <jejeco75@gmail.com>
2023-05-12 23:00:19 -0300

    versao 1.9

--- app.py
+++ app.py
@@ -174,23 +174,23 @@ elif type_problem == 'Cota Execução Financeira' and file != None:
 
 	if visualizar:
 
-		# try:
+		try:
 	
-		data = cota_execucao_financeira(file = file, skip = info_skip)
-
-		st.dataframe(data)
-
-		st.success('Arquivo lido com sucesso!')
-		
-		st.download_button(
-			label = 'Baixar Planilha',
-			data = export_excel(data = data),
-			file_name = type_problem + ' ' + str(int(datetime.now().timestamp())) + '.xlsx'
-		)
+			data = cota_execucao_financeira(file = file, skip = info_skip)
 	
-		# except:
+			st.dataframe(data)
 	
-			# st.error('Erro ao tentar ler o arquivo, verifique a quantidade de linhas a pular.')
+			st.success('Arquivo lido com sucesso!')
+			
+			st.download_button(
+				label = 'Baixar Planilha',
+				data = export_excel(data = data),
+				file_name = type_problem + ' ' + str(int(datetime.now().timestamp())) + '.xlsx'
+			)
+	
+		except:
+	
+			st.error('Erro ao tentar ler o arquivo, verifique a quantidade de linhas a pular.')
 
 elif type_problem == 'Extrato Bancário' and file != None:
 

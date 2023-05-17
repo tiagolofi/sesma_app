8aa7d0ac456daa4743149517d19d4c3d4c61ec8f
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-05-17 14:37:35 -0300

    columns listar pp

--- utils.py
+++ utils.py
@@ -584,7 +584,10 @@ def situacao_pp(file: str, skip: int):
 	data = data.dropna(how='all', axis='columns')
 	data = data.dropna(thresh=2, axis='index')
 
-	# data.columns = ['PreparacaoPagamento', 'OrdemBancaria', 'Favorecido', 'NotaEmpenho', 'DetalheNE', 'Valor', 'SituacaoPP']
+	data.columns = [
+		'PreparacaoPagamento', 'OrdemBancaria', 'Favorecido', 'NotaEmpenho', 
+		'CpfCnpj', 'Fonte', 'GND', 'Data', 'Valor', 'SituacaoPP'
+	]
 
 	return data
 

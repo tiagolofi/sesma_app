e05a1d47ce75ec9123cd0f715a5086797fcfd5e8
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-05 16:29:51 -0300

    teste

--- utils.py
+++ utils.py
@@ -956,7 +956,7 @@ def nota_pre_empenho_celula(file: str, skip: int):
 
 	df = df[df[2].astype(str).str.contains('2023PE')]
 
-	df = df.ffill()
+	empenhos = empenhos.bfill()
 
 	df = concat([df, empenhos], axis = 1)
 

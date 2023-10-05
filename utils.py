a83202bb38cb421e41ebd2e83a50a2a7dcf0c77f
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-05 14:51:06 -0300

    teste

--- utils.py
+++ utils.py
@@ -941,7 +941,7 @@ def nota_pre_empenho_celula(file: str, skip: int):
 	)
 
 	# df = df.dropna(how='all', axis='columns')
-	df = df.dropna(how='all', axis='index')
+	# df = df.dropna(how='all', axis='index')
 
 	# df = df[df[2].astype(str).str.contains('2023PE')]
 

8f4b2862ca3b511f2db0868b22c582df9467dafe
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-31 19:54:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ with c3:
 	st.markdown(
 	'''
 	<iframe 
-		style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6knNdMEAqhuP7ZCe9dXHKk?utm_source=generator" 
+		style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6knNdMEAqhuP7ZCe9dXHKk?utm_source=generator&theme=0" 
 	 	width="100%" height="352" frameBorder="0" allowfullscreen="" 
 	  	allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
 	</iframe>

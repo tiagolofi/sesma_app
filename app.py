2fc182d8b7e2bfded716be365a0918e18b877600
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-31 19:52:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ st.markdown(
 '''
 <iframe 
 	style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6knNdMEAqhuP7ZCe9dXHKk?utm_source=generator" 
- 	width="100%" height="352" frameBorder="0" allowfullscreen="" 
+ 	width="100%" height="152" frameBorder="0" allowfullscreen="" 
   	allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
 </iframe>
 ''', unsafe_allow_html=True

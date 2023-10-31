c65436fc986ef0442a10a240dee703ea795d4981
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-31 19:50:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ st.markdown(
  	width="100%" height="352" frameBorder="0" allowfullscreen="" 
   	allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
 </iframe>
-'''
+''', unsafe_allow_html=True
 )
 
 st.sidebar.write('''**Instruções de Linhas**''')

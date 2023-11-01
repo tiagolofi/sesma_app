8cc966fbb7e598d2e49e353d072596f2bc43664a
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-11-01 13:49:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ with c1:
 			'''
 			<iframe 
 				style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6knNdMEAqhuP7ZCe9dXHKk?utm_source=generator&theme=0" 
-			 	width="50%" height="370" frameBorder="0" allowfullscreen="" 
+			 	width="70%" height="370" frameBorder="0" allowfullscreen="" 
 			  	allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
 			</iframe>
 			''', unsafe_allow_html=True

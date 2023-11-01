7f8e796643afa87e947323226587a5cbc3b8e4b2
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-11-01 14:18:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ with c2:
 	st.markdown(
 	'''
  	<iframe 
-  		width="560" height="315" src="https://www.youtube-nocookie.com/embed/tYzMYcUty6s?si=6mysQ7urBpKjRYv0" 
+  		width="100%" height="315" src="https://www.youtube-nocookie.com/embed/tYzMYcUty6s?si=6mysQ7urBpKjRYv0" 
   		title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
    		encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
 	</iframe>

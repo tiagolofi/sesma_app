6d1fad861a2e2592dfd449440e61126c2878f9ed
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-11-01 14:42:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ with c1:
 	
 	with st.expander('Estação de Entretenimento'):
 
-		tabs1, tabs2, tabs3, tabs4 = st.tabs(['Spotify', 'Youtube Music', 'Transmmissão Looney Tunes', 'Futebol ao vivo'])
+		tabs1, tabs2, tabs3, tabs4 = st.tabs(['Spotify', 'Youtube Music', 'Transmmissão Looney Tunes', 'Vasco ao vivo'])
 		
 		with tabs1:
 
@@ -91,11 +91,10 @@ with c1:
 
 			st.markdown(
 			'''
-   			<iframe 
-      				width="100%" height="315"
-      				src="https://canaisplay.com/categoria/futebol-ao-vivo/">
-	  		</iframe>
-    			''', unsafe_allow_html=True
+			<a href="https://canaisplay.com/categoria/times/vasco/">
+				<img src="vasco.png" alt="Vasco" style="width:42px;height:42px;">
+			</a>
+   			''', unsafe_allow_html=True
 			)
 
 with c3:
@@ -241,23 +240,23 @@ elif type_problem == 'Crédito Disponível' and file != None:
 
 	if visualizar:
 
-		# try:
+		try:
 	
-		data = credito(file = file, skip = info_skip)
-
-		st.dataframe(data)
-
-		st.success('Arquivo lido com sucesso!')
-
-		st.download_button(
-			label = 'Baixar Planilha',
-			data = export_excel(data = data),
-			file_name = type_problem + ' ' + str(int(datetime.now().timestamp())) + '.xlsx'
-		)
-
-		# except:
+			data = credito(file = file, skip = info_skip)
 	
-			# st.error('Erro ao tentar ler o arquivo, verifique a quantidade de linhas a pular.')
+			st.dataframe(data)
+	
+			st.success('Arquivo lido com sucesso!')
+	
+			st.download_button(
+				label = 'Baixar Planilha',
+				data = export_excel(data = data),
+				file_name = type_problem + ' ' + str(int(datetime.now().timestamp())) + '.xlsx'
+			)
+
+		except:
+	
+			st.error('Erro ao tentar ler o arquivo, verifique a quantidade de linhas a pular.')
 		
 elif type_problem == 'FNS' and file != None:
 
